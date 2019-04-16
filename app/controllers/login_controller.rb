76b4db5dff645790c29175309e9437046aeb5202
Benjamin Jehl <benjamin.k.jehl@gmail.com>
2019-04-15 21:10:38 -0400

    change token to have payload instead of render json

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,11 +4,8 @@ class LoginController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
-      auth_token = Auth.generate(user_id: user.id)
-      render json: { auth_token: auth_token,
-                     first_name: user.first_name,
-                     last_name: user.last_name,
-                     email: user.email }
+      auth_token = Auth.generate(user_id: user.id, first_name: user.first_name)
+      render json: { auth_token: auth_token }
     else
       render json: { errors: ['Cannot log it', 'Unauthorized'] }, status: :unauthorized
     end
--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -4,11 +4,8 @@ class RegisterController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      auth_token = Auth.generate(user_id: user.id)
-      render json: { auth_token: auth_token,
-                     first_name: user.first_name,
-                     last_name: user.last_name,
-                     email: user.email }
+      auth_token = Auth.generate(user_id: user.id, first_name: user.first_name)
+      render json: { auth_token: auth_token }
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
