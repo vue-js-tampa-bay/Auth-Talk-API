808f8dd9f0d4d701127b60607f348e8e0e275a1b
benj <benj@knowbe4.com>
2019-04-24 09:44:54 -0400

    update api to remove unneeded code

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,7 +13,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
              headers: :any,
-             credentials: true,
              methods: %i[get post put patch delete options head]
   end
 end
